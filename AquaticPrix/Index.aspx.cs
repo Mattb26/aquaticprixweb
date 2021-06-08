f39fba98ba19d39eb64ec30748dc54af6b5abac9
bostero854 <bostero854@gmail.com>
2021-06-08 00:43:53 -0300

    llamado

--- AquaticPrix/Index.aspx.cs
+++ AquaticPrix/Index.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AquaticPrix.Entidades;
+using System;
+using System.Web.Services;
 
 namespace AquaticPrix
 {
@@ -7,6 +9,36 @@ namespace AquaticPrix
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        [WebMethod]
+        public static bool Alta(Usuario usuario)
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [WebMethod]
+        //public static bool AgregarContacto(Entidades.Contacto contacto)
+        public static bool AgregarContacto(string nombre, string correo, string asjunto, string msj)
+        {
+            try
+            {
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
---
+++ AquaticPrix/assets/js/Index.js
@@ -0,0 +1,53 @@
+﻿function AgregarContacto() {
+
+    var Nombre = $("input[ID$='txtNombreContac']").val();
+    var CorreoElectronico = $("input[ID$='txtemailContac']").val();
+    var Asunto = $("input[ID$='txtAsuntoContac']").val();
+    var Mensaje = $("input[ID$='txtMsjContac']").val();
+    console.log("ingreso");
+
+    //var Contacto = {
+    //   Nombre = $("input[ID$='txtNombreContac']").val(),
+    //   CorreoElectronico = $("input[ID$='txtemailContac']").val(),
+    //   Asunto = $("input[ID$='txtAsuntoContac']").val(),
+    //   Mensaje = $("input[ID$='txtMsjContac']").val()
+    //};
+    //string nombre, string correo, string asjunto, string msj
+
+    //console.log(Contacto);
+
+    try {
+
+        $.ajax({
+            type: "POST",
+            url: 'Index.aspx/AgregarContacto',
+            data: JSON.stringify({ nombre: Nombre, correo: CorreoElectronico, asjunto: Asunto, msj:Mensaje}),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: Estado,
+            failure: function (response) {
+                alert(response);
+            },
+            error: function (response) {
+                alert(response);
+            }
+        });
+
+    } catch (e) {
+        console.log(e);
+        alert(e);
+    }
+
+
+    return false;
+}
+
+function Estado(response) {
+
+   
+
+    if (response.d) {
+      
+    }
+
+}
